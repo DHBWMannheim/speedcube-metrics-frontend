806f570917f14197754c80b9bd160b11e994044f
Michael <m.angermeier@gmx.de>
2020-09-01 15:46:39 +0200

    Inital Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { NbLayoutModule, NbThemeModule } from '@nebular/theme';
+import { AnalysisComponent } from './analysis/analysis.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompetitionComponent } from './competition/competition.component';
+import { LoginComponent } from './login/login.component';
+import { OverviewComponent } from './overview/overview.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { TrainingComponent } from './training/training.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    OverviewComponent,
+    TrainingComponent,
+    CompetitionComponent,
+    AnalysisComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      { path: 'login', component: LoginComponent },
+      { path: 'registration', component: RegistrationComponent },
+      { path: 'competition', component: CompetitionComponent },
+      { path: 'overview', component: OverviewComponent },
+      { path: 'training', component: TrainingComponent },
+      { path: 'analysis', component: AnalysisComponent },
+    ]),
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbLayoutModule,
+    NbEvaIconsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
