4e56d48874dab89e3d674e12a9d72261177de9bb
Michael <m.angermeier@gmx.de>
2020-09-01 16:45:43 +0200

    Navigation Bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
-import { NbLayoutModule, NbThemeModule } from '@nebular/theme';
+import { NbContextMenuModule, NbLayoutModule, NbMenuModule, NbThemeModule } from '@nebular/theme';
 import { AnalysisComponent } from './analysis/analysis.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CompetitionComponent } from './competition/competition.component';
 import { LoginComponent } from './login/login.component';
+import { MainBackgroundComponent } from './main-background/main-background.component';
 import { OverviewComponent } from './overview/overview.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { TrainingComponent } from './training/training.component';
@@ -22,23 +23,27 @@ import { TrainingComponent } from './training/training.component';
     OverviewComponent,
     TrainingComponent,
     CompetitionComponent,
-    AnalysisComponent
+    AnalysisComponent,
+    MainBackgroundComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       { path: 'login', component: LoginComponent },
       { path: 'registration', component: RegistrationComponent },
-      { path: 'competition', component: CompetitionComponent },
-      { path: 'overview', component: OverviewComponent },
-      { path: 'training', component: TrainingComponent },
-      { path: 'analysis', component: AnalysisComponent },
+      { path: 'main', component: MainBackgroundComponent },
+      { path: 'main/competition', component: CompetitionComponent },
+      { path: 'main/overview', component: OverviewComponent },
+      { path: 'main/training', component: TrainingComponent },
+      { path: 'main/analysis', component: AnalysisComponent },
     ]),
     AppRoutingModule,
     BrowserAnimationsModule,
     NbThemeModule.forRoot({ name: 'default' }),
     NbLayoutModule,
-    NbEvaIconsModule
+    NbEvaIconsModule,
+    NbContextMenuModule,
+    NbMenuModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/main-background/main-background.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-background',
+  templateUrl: './main-background.component.html',
+  styleUrls: ['./main-background.component.css']
+})
+export class MainBackgroundComponent implements OnInit {
+  items = [
+    { title: 'Overview' },
+    { title: 'Training' },
+    { title: 'Competition' },
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+}
