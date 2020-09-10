998131bc2a578e6a090aaff71dfb34a89e609925
Jan Grübener <s180690@student.dhbw-mannheim.de>
2020-09-10 16:21:10 +0200

    implemented Login and Registration Funcionality

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,13 +10,18 @@ import { auth } from 'firebase/app';
 export class LoginComponent implements OnInit {
   constructor(public authService: AngularFireAuth) {}
 
-  async login() {
-    //await this.authService.signInWithEmailAndPassword('', '');
+  async login(username, password) {
+    await this.authService.signInWithEmailAndPassword(username, password);
+  }
+  async loginWithGoogle(){
     await this.authService.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+  async loginWithGitHub(){
+    await this.authService.signInWithPopup(new auth.GithubAuthProvider());
   }
 
   async register() {
-    await this.authService.createUserWithEmailAndPassword('', '');
+    //await this.authService.createUserWithEmailAndPassword('', '');
   }
 
   ngOnInit(): void {}
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+  value = '';
+  onEnter(value: string){
+    this.value = value
+  }
 
-  constructor() { }
+  constructor(public authService: AngularFireAuth) { }
 
   ngOnInit(): void {
+  }
+  async loginWithGoogle(){
+    await this.authService.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+  async loginWithGitHub(){
+    await this.authService.signInWithPopup(new auth.GithubAuthProvider());
+  }
+
+  async register() {
+    // await this.authService.createUserWithEmailAndPassword('', '');
+    console.log(this.value)
   }
 
 }
