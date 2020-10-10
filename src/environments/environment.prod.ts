9548550c0873d4f416403f445c238b4c8a184291
Troy Kessler <troy.kessler99@gmail.com>
2020-10-10 16:37:06 +0200

    added firebase to prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBXygi3erbqBLhCnep5_oDkhtny8hx5q8M',
+    authDomain: 'rubiks-cube-2fbba.firebaseapp.com',
+    databaseURL: 'https://rubiks-cube-2fbba.firebaseio.com',
+    projectId: 'rubiks-cube-2fbba',
+    storageBucket: 'rubiks-cube-2fbba.appspot.com',
+    messagingSenderId: '473241245332',
+    appId: '1:473241245332:web:38f804967bdadebc770681',
+    measurementId: 'G-JN6GN97T9N',
+  },
 };
